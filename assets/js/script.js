71f72dace0ae26ec875cfa21099fcb3b51082f93
ragnarok <angelriera1796@gmail.com>
2022-10-12 16:39:13 -0400

    Updates

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+// modal
+const modal = document.getElementsByClassName("modal")[0]
+const handleModal = () => {
+    modal.classList.toggle("hidden")
+}
+
+// form by step
+
+
+
+
+let current = 1
+
+const progres_bar = document.getElementById("bar_progres")
+const form_step = document.getElementById("form_step")
+
+console.log(form_step);
+
+const widthSlider = form_step.children[0].offsetWidth
+
+/* pasos */
+const handleStep = (bool) => {
+    if (bool == true) {
+        form_step.scrollLeft += widthSlider * 1
+        console.log(progres_bar);
+        progres_bar.children[current].classList.add("active")
+
+        current++
+
+    } else if (bool == false) {
+        form_step.scrollLeft -= widthSlider
+        current = current - 1
+        progres_bar.children[current].className = ""
+        console.log(progres_bar)
+
+
+    }
+}
+
+
+
+
+
